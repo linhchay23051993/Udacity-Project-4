725a82d87035b34e7c1792667aec7b2462033899
nguyenvanlinh23051993 <nguyenvanlinh23051993@gmail.com>
2024-07-21 12:19:22 +0000

    fix err map

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 
 function MovieList({ onMovieClick }) {
-  const [movies, setMovies] = useState([]);
-  console.log(`${process.env.REACT_APP_MOVIE_API_URL}/movies`);
+  const [listMovie, setMovies] = useState([]);
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
       setMovies(response.data.movies);
@@ -13,7 +12,7 @@ function MovieList({ onMovieClick }) {
 
   return (
     <ul>
-      {movies.map((movie) => (
+      {listMovie?.map((movie) => (
         <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
           {movie.title}
         </li>
