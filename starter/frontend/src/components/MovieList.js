0eb204866e58342f6b94b82f1b883a45c4859758
nguyenvanlinh23051993 <nguyenvanlinh23051993@gmail.com>
2024-07-21 12:25:50 +0000

    fix eslint

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 
 function MovieList({ onMovieClick }) {
-  const [listMovie, setMovies] = useState([]);
+  const [listMovies, setMovies] = useState([]);
 
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
@@ -13,7 +13,7 @@ function MovieList({ onMovieClick }) {
 
   return (
     <ul>
-      {listMovie?.map((movie) => (
+      {listMovies.map((movie) => (
         <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
           {movie.title}
         </li>
